4b32e5c366031d33157cc2cfb1ea4fbf570cfb84
zhongdiyang <1084575562@qq.com>
2024-02-05 10:25:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
-  // https://www.notion.so/zhongdiyang/5e2221726dbc4ac386c443ed49d9c20f
+  // https://zhongdiyang.notion.site/zhongdiyang/5e2221726dbc4ac386c443ed49d9c20f
   NOTION_PAGE_ID:
         process.env.NOTION_PAGE_ID || '5e2221726dbc4ac386c443ed49d9c20f',
   PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
